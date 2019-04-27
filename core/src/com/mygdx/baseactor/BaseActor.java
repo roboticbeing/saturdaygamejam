98ef4c28ac77b8893c11bc29e5fb1d50779c60d8
zibertv <zibertv@sheridancollege.ca>
2019-04-27 12:04:55 -0400

    idk

--- core/src/com/mygdx/baseactor/BaseActor.java
+++ core/src/com/mygdx/baseactor/BaseActor.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
