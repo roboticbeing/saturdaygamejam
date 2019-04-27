ad7a1ba24cdc1352b207ab10d06fcde89a67c204
zibertv <zibertv@sheridancollege.ca>
2019-04-27 12:52:37 -0400

    idk

--- android/src/com/mygdx/gamejam/AndroidLauncher.java
+++ android/src/com/mygdx/gamejam/AndroidLauncher.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.gamejam.SaturdayGameJam;
+
+import tampongame.TamponGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SaturdayGameJam(), config);
+		initialize(new TamponGame(), config);
 	}
 }
