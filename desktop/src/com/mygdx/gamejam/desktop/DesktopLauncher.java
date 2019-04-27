0f454aa4f11d40beca5cee99cbd2a73464b4dd17
zibertv <zibertv@sheridancollege.ca>
2019-04-27 08:35:17 -0400

    first commit

---
+++ desktop/src/com/mygdx/gamejam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gamejam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gamejam.SaturdayGameJam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SaturdayGameJam(), config);
+	}
+}
