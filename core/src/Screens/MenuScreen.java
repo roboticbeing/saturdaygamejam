57190b41d58348385fdd8b9587d51236d80ff40c
M.Conte <gonerbarage@gmail.com>
2019-04-27 12:25:15 -0400

    GameWorld&Renderer

--- core/src/Screens/MenuScreen.java
+++ core/src/Screens/MenuScreen.java
@@ -1,47 +1,31 @@
 package Screens;
 
-public class MenuScreen extends BaseScreen {
-    private static final MenuScreen ourInstance = new MenuScreen();
+import com.badlogic.gdx.Input.Keys;
 
-    public static MenuScreen getInstance() {
-        return ourInstance;
-    }
+public abstract class MenuScreen extends BaseScreen {
 
-    private MenuScreen() {
-    }
-
-    @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void render(float delta) {
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
+    public void initialize()
+    {
+        /*
+        BaseActor ocean = new BaseActor(0,0, mainStage);
+        ocean.loadTexture( "assets/water.jpg" );
+        ocean.setSize(800,600);
+        BaseActor title = new BaseActor(0,0, mainStage);
+        title.loadTexture( "assets/starfish-collector.png" );
+        title.centerAtPosition(400,300);
+        title.moveBy(0,100);
+        BaseActor start = new BaseActor(0,0, mainStage);
+        start.loadTexture( "assets/message-start.png" );
+        start.centerAtPosition(400,300);
+        start.moveBy(0,-100);
+        */
 
     }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
+    public void update(float dt)
+    {
+        /*
+        if (Gdx.input.isKeyPressed(Keys.S))
+            StarfishGame.setActiveScreen( new LevelScreen() );
+        */
     }
 }
