bf26f63daacb4d5cef9c770ab33d950822626531
zibertv <zibertv@sheridancollege.ca>
2019-04-27 13:37:36 -0400

    idk

--- core/src/com/mygdx/baseactor/Tampon.java
+++ core/src/com/mygdx/baseactor/Tampon.java
@@ -7,9 +7,8 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 public class Tampon extends BaseActor {
     public Tampon(float x, float y, Stage stage) {
         super(x, y, stage);
-        String[] fileNames = {"tampon-1.png", "tampon-2.png", "tampon-3"};
-//        loadAnimationFromFiles(fileNames, 0.1f, true);
-        loadTexture("tampon.png");
+        String[] fileNames = {"tampon-1.png", "tampon-2.png", "tampon-3.png"};
+        loadAnimationFromFiles(fileNames, 0.5f, true);
     }
 
     public void act(float deltaTime) {
